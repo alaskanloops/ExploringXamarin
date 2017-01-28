bc8854a9fe64d66c690c57e29db930d19cca3d15
alaskanloops <alaskanloops@gmail.com>
2017-01-28 12:33:30 -0900

    Initial Commit

---
+++ ExploringXamarin.Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ExploringXamarin.Backend.Startup))]
+
+namespace ExploringXamarin.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
